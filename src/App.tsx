adef25c1b6d2a6c1e4e2f122799ab7b01c3bfbbd
vjvl95 <vjvl95@naver.com>
2023-01-31 17:04:41 +0900

    feat: pagenation 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import Header from './component/Header';
 import ListTable from './component/ListTable';
 import { useEffect, useState } from 'react';
 import React from 'react';
+import Pagenation from './component/Pagenation';
 
 export default function App() {
   const [searchCategory, setSearchCategory] = useState('all');
   const [productList, setProductList] = useState<any>([]);
   const [totalCount, setTotalCount] = useState<number>(0);
+  const [limit, setLimit] = useState<number>(10);
+  const [page, setPage] = useState<number>(1);
   const getDataAPI = async () => {
-    const requestURL = 'https://dummyjson.com/products?limit=50';
+    const requestURL = `https://dummyjson.com/products?limit=${limit}&skip=${
+      limit * (page - 1)
+    }`;
     const res = await fetch(requestURL);
     const result = await res.json();
     setProductList(result.products);
@@ -18,7 +23,7 @@ export default function App() {
 
   useEffect(() => {
     getDataAPI();
-  }, []);
+  }, [page, limit]);
   return (
     <div className='container'>
       <Header
@@ -29,6 +34,13 @@ export default function App() {
         검색된 데이터 : {totalCount} 건
       </section>
       <ListTable productList={productList}></ListTable>
+      <Pagenation
+        total={totalCount}
+        limit={limit}
+        setLimit={setLimit}
+        page={page}
+        setPage={setPage}
+      />
     </div>
   );
 }
