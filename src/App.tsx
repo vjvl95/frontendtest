0378e9bc3f3b0414e15a3b7f83e72b787325e1b0
vjvl95 <vjvl95@naver.com>
2023-01-31 14:41:06 +0900

    refactor: 데이터 받아서 자식 컴포넌트에 넘기는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,33 @@ import './App.css';
 import Header from './component/Header';
 import ListTable from './component/ListTable';
 import { useEffect, useState } from 'react';
+import React from 'react';
 
 export default function App() {
   const [searchCategory, setSearchCategory] = useState('all');
   const [productList, setProductList] = useState<any>([]);
+  const [totalCount, setTotalCount] = useState<number>(0);
   const getDataAPI = async () => {
     const requestURL = 'https://dummyjson.com/products?limit=50';
     const res = await fetch(requestURL);
     const result = await res.json();
     setProductList(result.products);
+    setTotalCount(result.total);
   };
 
   useEffect(() => {
     getDataAPI();
-    console.log(productList);
   }, []);
   return (
-    productList && (
-      <div className='container'>
-        <Header
-          searchCategory={searchCategory}
-          setSearchCategory={setSearchCategory}
-        />
-        gssdf
-        <ListTable productList={productList}></ListTable>
-      </div>
-    )
+    <div className='container'>
+      <Header
+        searchCategory={searchCategory}
+        setSearchCategory={setSearchCategory}
+      />
+      <section className='totalsection'>
+        검색된 데이터 : {totalCount} 건
+      </section>
+      <ListTable productList={productList}></ListTable>
+    </div>
   );
 }
