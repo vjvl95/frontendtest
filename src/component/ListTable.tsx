ae56227279d719366bf2a0060c0bc964b7bc3e97
vjvl95 <vjvl95@naver.com>
2023-02-02 01:07:01 +0900

    refactor: 데이터가 존재하지 않을 때 문구 추구

--- src/component/ListTable.tsx
+++ src/component/ListTable.tsx
@@ -13,39 +13,52 @@ export default function ListTable({ productList }: List) {
   const offset = (page - 1) * limit;
   return (
     <div className='tablecontainer'>
-      <table style={{ width: '100%' }}>
-        <thead className='tablehead'>
-          <tr>
-            <th>상품번호</th>
-            <th>상품명</th>
-            <th>브랜드</th>
-            <th>상품내용</th>
-            <th>가격</th>
-            <th>평점</th>
-            <th>재고</th>
-          </tr>
-        </thead>
+      {productList.length === 0 ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            fontSize: '40px',
+            fontWeight: '700',
+          }}
+        >
+          데이터가 존재하지 않습니다.
+        </div>
+      ) : (
+        <table style={{ width: '100%' }}>
+          <thead className='tablehead'>
+            <tr>
+              <th>상품번호</th>
+              <th>상품명</th>
+              <th>브랜드</th>
+              <th>상품내용</th>
+              <th>가격</th>
+              <th>평점</th>
+              <th>재고</th>
+            </tr>
+          </thead>
 
-        <tbody className='tablebody'>
-          {productList.slice(offset, offset + limit).map((v: any) => {
-            return (
-              <tr className='tableItem'>
-                <td>{v.id}</td>
-                <td>{v.title}</td>
-                <td>{v.brand}</td>
-                <td>
-                  {v.description.length > 40
-                    ? v.description.slice(0, 40) + '....'
-                    : v.description}
-                </td>
-                <td>{v.price}</td>
-                <td>{v.rating}</td>
-                <td>{v.stock}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          <tbody className='tablebody'>
+            {productList.slice(offset, offset + limit).map((v: any) => {
+              return (
+                <tr className='tableItem'>
+                  <td>{v.id}</td>
+                  <td>{v.title}</td>
+                  <td>{v.brand}</td>
+                  <td>
+                    {v.description.length > 40
+                      ? v.description.slice(0, 40) + '....'
+                      : v.description}
+                  </td>
+                  <td>{v.price}</td>
+                  <td>{v.rating}</td>
+                  <td>{v.stock}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
